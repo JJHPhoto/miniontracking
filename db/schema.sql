4f3e183f51977c6be1d8f59d7ac5ed1e292ad83d
Joshua J. Haller <phone@joshuajhaller.com>
2020-12-22 20:23:29 -0800

    schema updated and working in mysql workbench.

--- db/schema.sql
+++ db/schema.sql
@@ -2,20 +2,27 @@ DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 USE employees;
 
+create table department (
+id int auto_increment,
+name varchar(30),
+primary key (id)
+);
+
+create table role (
+id int auto_increment,
+title varchar(30),
+salary decimal(8, 2),
+department_id int,
+primary key (id),
+foreign key (id) references department(id)
+);
+
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
-);
-CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-);
-CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+id INT auto_increment,
+first_name varchar(30),
+last_name varchar(30),
+role_id int,
+manager_id int,
+primary key (id),
+foreign key (id) references role(id)
 );
