dd6059b31e42ed22667b069229e0d05170a82e36
Joshua J. Haller <phone@joshuajhaller.com>
2020-12-22 20:32:59 -0800

    Seed and schema files updated.

--- db/schema.sql
+++ db/schema.sql
@@ -3,18 +3,18 @@ CREATE DATABASE employees;
 USE employees;
 
 create table department (
-id int auto_increment,
+department_id int auto_increment,
 name varchar(30),
-primary key (id)
+primary key (department_id)
 );
 
 create table role (
-id int auto_increment,
+role_id int auto_increment,
 title varchar(30),
 salary decimal(8, 2),
 department_id int,
-primary key (id),
-foreign key (id) references department(id)
+primary key (role_id),
+foreign key (department_id) references department(department_id)
 );
 
 CREATE TABLE employee (
@@ -24,5 +24,5 @@ last_name varchar(30),
 role_id int,
 manager_id int,
 primary key (id),
-foreign key (id) references role(id)
+foreign key (role_id) references role(role_id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -7,18 +7,19 @@ VALUES
   ("Engineering"),
   ("Finance"),
   ("Legal");
-
+  
 INSERT INTO role
-    (title, salary, department_id)
+    (title, salary, role_id)
 VALUES
     ("Sales Lead", 100000, 1),
-    ("Salesperson", 80000, 1),
-    ("Lead Engineer", 150000, 2),
-    ("Account Manager", 120000, 2),
-    ("Accountant", 160000, 3),
-    ("Accountant", 125000, 3),
-    ("Legal Team Lead", 250000, 4),
-    ("Lawyer", 190000, 4);
+    ("Salesperson", 80000, 2),
+    ("Lead Engineer", 150000, 3),
+    ("Account Manager", 120000, 4),
+    ("Accountant", 160000, 5),
+    ("Accountant", 125000, 6),
+    ("Legal Team Lead", 250000, 7),
+    ("Lawyer", 190000, 8);
+    
 
 INSERT INTO employee
   (first_name, last_name, role_id, manager_id)
