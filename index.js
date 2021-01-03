c1922962e64f714995160e4b56e6557c9898823a
Joshua J. Haller <phone@joshuajhaller.com>
2021-01-02 19:14:48 -0800

    Forgot about choices array.

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function askForAction() {
         "Remove Employee",
         "Update Employee Role",
         "Update Employee Manager",
+        "View All Roles",
+        "Add Role",
+        "Remove Role",
+        "View All Departments",
+        "Add Department",
+        "Remove Department",
       ],
     })
     .then((response) => {
