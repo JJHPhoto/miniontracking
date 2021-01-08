d4c9b8dacc6d5e650bbbe55d14d676d8a82aa22c
Joshua J. Haller <phone@joshuajhaller.com>
2021-01-07 18:22:52 -0800

    Working views look as I want them to.

--- db/index.js
+++ db/index.js
@@ -3,16 +3,18 @@ const connection = require("./connection");
 module.exports = {
   getEmployees() {
     return connection.query(
-      "SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', employee.role_id AS 'Role ID', employee.role AS 'Role', employee.department AS 'Department', employee.manager_id AS 'Manager ID', employee.manager_name AS 'Manager Name' FROM employee "
+      "SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', employee.role_id AS 'Role ID', employee.role AS 'Role', employee.department AS 'Department', employee.manager_id AS 'Manager ID', employee.manager_name AS 'Manager Name' FROM employee ORDER BY last_name"
     );
   },
   getEmployeesByDepartment() {
     return connection.query(
-      "SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', employee.department AS 'Department' FROM employee LEFT JOIN role ON employee.role_id = role.role_id;"
+      "SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', employee.department AS 'Department' FROM employee LEFT JOIN role ON employee.role_id = role.role_id ORDER BY department desc"
     );
   },
   getEmployeesByManager() {
-    return connection.query("SELECT * FROM employee ORDER BY manager_name;");
+    return connection.query(
+      "SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', employee.role_id AS 'Role ID', employee.role AS 'Role', employee.manager_id AS 'Manager ID', employee.manager_name AS 'Manager Name' FROM employee ORDER BY manager_name desc"
+    );
   },
   addNewEmployee(data) {
     return connection.query("INSERT INTO employee SET ?", data);
@@ -28,7 +30,9 @@ module.exports = {
     ]);
   },
   getDepartments() {
-    return connection.query("SELECT * FROM department");
+    return connection.query(
+      "SELECT department.department_id AS 'Department ID', department.name AS 'Name' FROM department"
+    );
   },
   getRoles() {
     return connection.query(
